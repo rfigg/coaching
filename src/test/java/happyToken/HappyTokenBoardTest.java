150777311024fd5c6f3cb14b110d78335ba178e1
Ryan Figg <ryan.figg@gmail.com>
2014-02-05 17:39:48 -0600

    First good set of tests implemented.

--- src/test/java/happyToken/HappyTokenBoardTest.java
+++ src/test/java/happyToken/HappyTokenBoardTest.java
@@ -17,16 +17,14 @@ public class HappyTokenBoardTest {
 		// test board creation
 		HappyTokenBoard myBoard = new HappyTokenBoard();
 
-		// test empty board
-
 		// test bad token adds
 		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.BLACK, 5));
 		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.WHITE, -1));
 		System.out.println(Arrays.toString(myBoard.viewStatus()));
 		
-
-		myBoard.reset();
 		// test empty board
+		myBoard.reset();
+		assertEquals("Board should match", "[0, 0, 0, 0, 0, 0, -1]", Arrays.toString(myBoard.viewStatus()));
 
 		// test good token adds
 		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 0));
@@ -47,14 +45,47 @@ public class HappyTokenBoardTest {
 		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 2));
 
 		System.out.println(Arrays.toString(myBoard.viewStatus()));
+		assertEquals("Board should match", "[2, 3, 2, 4, 2, 1, -1]", Arrays.toString(myBoard.viewStatus()));
 
 
 		//test board position logic through mixed good and bad adds
 		myBoard.reset();
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 0));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.BLACK, 0));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 0));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 0));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 0));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.WHITE, 0));
 
+		System.out.println(Arrays.toString(myBoard.viewStatus()));
+		assertEquals("Board should match", "[2, 2, 0, 0, 0, 0, -1]", Arrays.toString(myBoard.viewStatus()));
+
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 1));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.WHITE, 1));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 1));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 1));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.BLACK, 1));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 1));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 1));
+
+		System.out.println(Arrays.toString(myBoard.viewStatus()));
+		assertEquals("Board should match", "[2, 2, 3, 2, 0, 0, -1]", Arrays.toString(myBoard.viewStatus()));
+
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 2));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 2));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 2));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.BLACK, 2));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.WHITE, 2));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 2));
+		assertEquals("Should not be able to add token", false, myBoard.addTokenToPosition(TokenColor.WHITE, 2));
+		assertEquals("Should be able to add token", true, myBoard.addTokenToPosition(TokenColor.BLACK, 2));
+
+		System.out.println(Arrays.toString(myBoard.viewStatus()));
+		assertEquals("Board should match", "[2, 2, 3, 2, 3, 3, -1]", Arrays.toString(myBoard.viewStatus()));
 
 
 		// test email functionality
+		myBoard.reset();
 		for (int i = 0; i<10; i++) {
 			if (i % 2 == 0)
 				myBoard.addTokenToPosition(TokenColor.WHITE, 0);
@@ -63,6 +94,7 @@ public class HappyTokenBoardTest {
 		}
 
 		System.out.println(Arrays.toString(myBoard.viewStatus()));
+		assertEquals("Board should match", "[5, 5, 0, 0, 0, 0, -1]", Arrays.toString(myBoard.viewStatus()));
 
 		System.out.println("Done with test!");
 	}
