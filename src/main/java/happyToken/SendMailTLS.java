c570584e7a12ce741b3f107e5efb88170e24d23e
Ryan Figg <ryan.figg@gmail.com>
2014-02-05 14:29:50 -0600

    Fixing amazon smtp hostname to use SES.

--- src/main/java/happyToken/SendMailTLS.java
+++ src/main/java/happyToken/SendMailTLS.java
@@ -21,7 +21,7 @@ public class SendMailTLS {
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.host", "email-smtp.us-east-1.amazonaws.com");
 		props.put("mail.smtp.port", "587");
  
 		Session session = Session.getInstance(props,
